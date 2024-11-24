ed26cb84d23e27f8b3becac26a7408334072a16c
Stephie-cpu <wahomestephanie@zetech.ac.ke>
2024-11-25 00:24:10 +0300

    structures.c

--- structures.c
+++ structures.c
@@ -1,29 +1,16 @@
-//Book structure
 #include <stdio.h>
-#include <stdio.h>//scanf(),printf()
-#include <string.h> //strcpy()
-struct book {
+#include <string.h>
+
+struct Book {
     char title[30];
-char author[30];
-int publication_year;
-char ISBN[13];
-float price; 
+    char author[30];
+    int publicationYear;
+    char ISBN[13];
+    float price;
+};  // Added semicolon here
 
-} book1 ;
-int main(){
-strcpy(book1.title,"Introduction to C Programming");
-strcpy(book1.author,"John Smith");
-book1.publication_year = 2022;
-strcpy(book1.ISBN,"9780131103627");
-book1.price = 49.99;
-
-printf("title:%s\n",book1.title);
-printf("author:%s\n",book1.author);
-printf("publication_year:%d\n",book1.publication_year);
-printf("ISBN:%s\n",book1.ISBN);
-printf("price:%.2f\n",book1.price);
-
-
-return 0;
-    
+int main() {
+    struct Book book1 = {"Introduction to C Programming", "John Smith", 2022, "9780131103627", 49.99};
+    printf("%s %s %d %s %.2f\n", book1.title, book1.author, book1.publicationYear, book1.ISBN, book1.price);
+    return 0;
 }
