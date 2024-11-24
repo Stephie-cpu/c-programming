f6792ba4bbdbae52fe98857003bf3ff87d02936d
Stephie-cpu <wahomestephanie@zetech.ac.ke>
2024-11-24 23:46:12 +0300

    Area of a rectangle.c

---
+++ Area of a rectangle.c
@@ -0,0 +1,17 @@
+//Calculate the Area of a rectangle
+#include<stdio.h>//printf(),scanf()"/;
+int main(){ 
+           int length,width,area;
+           
+           printf("enter the length: ");
+           scanf("%d",&length);
+           
+           printf("enter the width: ");
+           scanf("%d",&width);
+           
+           area=length*width ;
+           
+           printf("the area of the rectangle is:%d",area);
+           return 0;
+    
+}
