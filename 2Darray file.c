edb323b4dd7c5901f6c941c32c722786293323a6
Stephie-cpu <wahomestephanie@zetech.ac.ke>
2024-11-25 08:33:33 +0300

    2Darray file.c

--- 2Darray file.c
+++ 2Darray file.c
@@ -2,18 +2,20 @@
 
 int main() {
     // Step i: Define an array data structure
-    // An array is a collection of variables of the same type stored in contiguous memory locations.
+    
 
-    // Step ii: Declare and initialize a 2D array named 'scores' with 2 rows and 2 columns
-    int scores[2][2] = { {65, 92}, {35, 70} };  // This creates a 2D array with specified values for each element.
 
-    // Step iii: Print the elements of the above array using a nested for loop
-    for (int i = 0; i < 2; i++) {               // Outer loop iterates over each row
-        for (int j = 0; j < 2; j++) {           // Inner loop iterates over each column in the current row
-            printf("%d ", scores[i][j]);        // Print the current element in the array
+    int scores[2][4] = {
+        {65, 92, 35, 70},
+        {84, 72, 59, 67}
+    };
+
+    int i,j;
+    for(i=0;i<2;i++){
+        for(j=0;j<4;j++){
+            printf("%d\n",scores[i][j]);
         }
-        printf("\n");                           // Print a new line after each row is completed
     }
 
-return 0;
+    return 0;
 }
